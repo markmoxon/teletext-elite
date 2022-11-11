b14b362bba7932eb72c5a1801faa65b250420c2d
Mark Moxon <mark@moxon.net>
2022-11-11 16:31:10 +0000

    Add scanner support

--- 1-source-files/main-sources/elite-source-flight.asm
+++ 1-source-files/main-sources/elite-source-flight.asm
@@ -32554,6 +32554,15 @@ LOAD_H% = LOAD% + P% - CODE%
 
 .SCAN
 
+                        \ --- Mod: Code added for Teletext Elite: ------------->
+
+ LDA QQ11               \ If this is not a space view, jump to SC5 to return
+ BNE SC5                \ from the subroutine, as the scanner is not being
+                        \ shown
+
+                        \ --- End of added code ------------------------------->
+
+
  LDA INWK+31            \ Fetch the ship's scanner flag from byte #31
 
  AND #%00010000         \ If bit 4 is clear then the ship should not be shown
@@ -32696,19 +32705,39 @@ LOAD_H% = LOAD% + P% - CODE%
                         \ instruction isn't required as we test both the maximum
                         \ and minimum below, but it might save a few cycles)
 
- CMP #194               \ If A >= 194, skip the following instruction, as 194 is
+                        \ --- Mod: Original Acornsoft code removed: ----------->
+
+\CMP #194               \ If A >= 194, skip the following instruction, as 194 is
+\BCS P%+4               \ the minimum allowed value of A
+\
+\LDA #194               \ A < 194, so set A to 194, the minimum allowed value
+\                       \ for the y-coordinate of our ship's dot
+\
+\CMP #247               \ If A < 247, skip the following instruction, as 246 is
+\BCC P%+4               \ the maximum allowed value of A
+\
+\.ld246
+\
+\LDA #246               \ A >= 247, so set A to 246, the maximum allowed value
+\                       \ for the y-coordinate of our ship's dot
+
+                        \ --- And replaced by: -------------------------------->
+
+ CMP #186               \ If A >= 186, skip the following instruction, as 186 is
  BCS P%+4               \ the minimum allowed value of A
 
- LDA #194               \ A < 194, so set A to 194, the minimum allowed value
+ LDA #186               \ A < 186, so set A to 186, the minimum allowed value
                         \ for the y-coordinate of our ship's dot
 
- CMP #247               \ If A < 247, skip the following instruction, as 246 is
+ CMP #255               \ If A < 255, skip the following instruction, as 255 is
  BCC P%+4               \ the maximum allowed value of A
 
 .ld246
 
- LDA #246               \ A >= 247, so set A to 246, the maximum allowed value
+ LDA #255               \ A >= 255, so set A to 255, the maximum allowed value
                         \ for the y-coordinate of our ship's dot
+
+                        \ --- End of replacement ------------------------------>
 
  STA Y1                 \ Store A in Y1, as it now contains the screen
                         \ y-coordinate for the ship's dot, clipped so that it
@@ -32739,164 +32768,192 @@ LOAD_H% = LOAD% + P% - CODE%
                         \
                         \ and we can get on with drawing the dot and stick
 
- PHP                    \ Store the flags (specifically the C flag) from the
-                        \ above subtraction
+                        \ --- Mod: Original Acornsoft code removed: ----------->
 
-.SC48
-
- PHA                    \ Store the stick height in A on the stack
-
+\PHP                    \ Store the flags (specifically the C flag) from the
+\                       \ above subtraction
+\
+\.SC48
+\
+\PHA                    \ Store the stick height in A on the stack
+\
 \JSR CPIX4              \ Draw a double-height dot at (X1, Y1). This also leaves
-                        \ the following variables set up for the dot's top-right
-                        \ pixel, the last pixel to be drawn (as the dot gets
-                        \ drawn from the bottom up):
-                        \
-                        \   SC(1 0) = screen address of the pixel's character
-                        \             block
-                        \
-                        \   Y = number of the character row containing the pixel
-                        \
-                        \   X = the pixel's number (0-3) in that row
-                        \
-                        \ We can use there as the starting point for drawing the
-                        \ stick, if there is one
-
- LDA CTWOS+1,X          \ Load the same mode 5 1-pixel byte that we just used
- AND COL                \ for the top-right pixel, and mask it with the same
- STA X1                 \ colour, storing the result in X1, so we can use it as
-                        \ the character row byte for the stick
-
- PLA                    \ Restore the stick height from the stack into A
-
- PLP                    \ Restore the flags from above, so the C flag once again
-                        \ reflects the sign of the stick height
-
- TAX                    \ Copy the stick height into X
-
- BEQ RTS                \ If the stick height is zero, then there is no stick to
-                        \ draw, so return from the subroutine (as RTS contains
-                        \ an RTS)
-
- BCC RTS+1              \ If the C flag is clear then the stick height in A is
-                        \ negative, so jump down to RTS+1
-
-.VLL1
-
-                        \ If we get here then the stick length is positive (so
-                        \ the dot is below the ellipse and the stick is above
-                        \ the dot, and we need to draw the stick upwards from
-                        \ the dot)
-
- DEY                    \ We want to draw the stick upwards, so decrement the
-                        \ pixel row in Y
-
- BPL VL1                \ If Y is still positive then it correctly points at the
-                        \ line above, so jump to VL1 to skip the following
-
- LDY #7                 \ We just decremented Y up through the top of the
-                        \ character block, so we need to move it to the last row
-                        \ in the character above, so set Y to 7, the number of
-                        \ the last row
-
- DEC SC+1               \ Decrement the high byte of the screen address to move
-                        \ to the character block above
-
-.VL1
-
- LDA X1                 \ Set A to the character row byte for the stick, which
-                        \ we stored in X1 above, and which has the same pixel
-                        \ pattern as the bottom-right pixel of the dot (so the
-                        \ stick comes out of the right side of the dot)
-
-                        \ --- Mod: Original Acornsoft code removed: ----------->
-
+\                       \ the following variables set up for the dot's top-right
+\                       \ pixel, the last pixel to be drawn (as the dot gets
+\                       \ drawn from the bottom up):
+\                       \
+\                       \   SC(1 0) = screen address of the pixel's character
+\                       \             block
+\                       \
+\                       \   Y = number of the character row containing the pixel
+\                       \
+\                       \   X = the pixel's number (0-3) in that row
+\                       \
+\                       \ We can use there as the starting point for drawing the
+\                       \ stick, if there is one
+\
+\LDA CTWOS+1,X          \ Load the same mode 5 1-pixel byte that we just used
+\AND COL                \ for the top-right pixel, and mask it with the same
+\STA X1                 \ colour, storing the result in X1, so we can use it as
+\                       \ the character row byte for the stick
+\
+\PLA                    \ Restore the stick height from the stack into A
+\
+\PLP                    \ Restore the flags from above, so the C flag once again
+\                       \ reflects the sign of the stick height
+\
+\TAX                    \ Copy the stick height into X
+\
+\BEQ RTS                \ If the stick height is zero, then there is no stick to
+\                       \ draw, so return from the subroutine (as RTS contains
+\                       \ an RTS)
+\
+\BCC RTS+1              \ If the C flag is clear then the stick height in A is
+\                       \ negative, so jump down to RTS+1
+\
+\.VLL1
+\
+\                       \ If we get here then the stick length is positive (so
+\                       \ the dot is below the ellipse and the stick is above
+\                       \ the dot, and we need to draw the stick upwards from
+\                       \ the dot)
+\
+\DEY                    \ We want to draw the stick upwards, so decrement the
+\                       \ pixel row in Y
+\
+\BPL VL1                \ If Y is still positive then it correctly points at the
+\                       \ line above, so jump to VL1 to skip the following
+\
+\LDY #7                 \ We just decremented Y up through the top of the
+\                       \ character block, so we need to move it to the last row
+\                       \ in the character above, so set Y to 7, the number of
+\                       \ the last row
+\
+\DEC SC+1               \ Decrement the high byte of the screen address to move
+\                       \ to the character block above
+\
+\.VL1
+\
+\LDA X1                 \ Set A to the character row byte for the stick, which
+\                       \ we stored in X1 above, and which has the same pixel
+\                       \ pattern as the bottom-right pixel of the dot (so the
+\                       \ stick comes out of the right side of the dot)
+\
 \EOR (SC),Y             \ Draw the stick on row Y of the character block using
 \STA (SC),Y             \ EOR logic
+\
+\DEX                    \ Decrement the (positive) stick height in X
+\
+\BNE VLL1               \ If we still have more stick to draw, jump up to VLL1
+\                       \ to draw the next pixel
+\
+\.RTS
+\
+\RTS                    \ Return from the subroutine
+\
+\                       \ If we get here then the stick length is negative (so
+\                       \ the dot is above the ellipse and the stick is below
+\                       \ the dot, and we need to draw the stick downwards from
+\                       \ the dot)
+\
+\INY                    \ We want to draw the stick downwards, so we first
+\                       \ increment the row counter so that it's pointing to the
+\                       \ bottom-right pixel in the dot (as opposed to the top-
+\                       \ right pixel that the call to CPIX4 finished on)
+\
+\CPY #8                 \ If the row number in Y is less than 8, then it
+\BNE P%+6               \ correctly points at the next line down, so jump to
+\                       \ VLL2 to skip the following
+\
+\LDY #0                 \ We just incremented Y down through the bottom of the
+\                       \ character block, so we need to move it to the first
+\                       \ row in the character below, so set Y to 0, the number
+\                       \ of the first row
+\
+\INC SC+1               \ Increment the high byte of the screen address to move
+\                       \ to the character block above
+\
+\.VLL2
+\
+\INY                    \ We want to draw the stick itself, heading downwards,
+\                       \ so increment the pixel row in Y
+\
+\CPY #8                 \ If the row number in Y is less than 8, then it
+\BNE VL2                \ correctly points at the next line down, so jump to
+\                       \ VL2 to skip the following
+\
+\LDY #0                 \ We just incremented Y down through the bottom of the
+\                       \ character block, so we need to move it to the first
+\                       \ row in the character below, so set Y to 0, the number
+\                       \ of the first row
+\
+\INC SC+1               \ Increment the high byte of the screen address to move
+\                       \ to the character block above
+\
+\.VL2
+\
+\LDA X1                 \ Set A to the character row byte for the stick, which
+\                       \ we stored in X1 above, and which has the same pixel
+\                       \ pattern as the bottom-right pixel of the dot (so the
+\                       \ stick comes out of the right side of the dot)
+\
+\EOR (SC),Y             \ Draw the stick on row Y of the character block using
+\STA (SC),Y             \ EOR logic
+\
+\INX                    \ Increment the (negative) stick height in X
+\
+\BNE VLL2               \ If we still have more stick to draw, jump up to VLL2
+\                       \ to draw the next pixel
+\
+\RTS                    \ Return from the subroutine
 
                         \ --- And replaced by: -------------------------------->
 
- NOP                    \ Pad the code out to the same length as in the original
- NOP
- NOP
- NOP
+ LDA SC                 \ Store the y-coordinate of the end of the stick in SC
+ PHA                    \ on the stack, as we overwrite SC in the drawing
+                        \ routines below
 
-                        \ --- End of replacement ------------------------------>
+ LDA Y1                 \ Set Y = (Y1 / 4) + 9
+ LSR A
+ LSR A
+ CLC
+ ADC #9
+ TAY
 
- DEX                    \ Decrement the (positive) stick height in X
+ LDA X1                 \ Set X = X1 / 4
+ LSR A
+ LSR A
+ TAX
 
- BNE VLL1               \ If we still have more stick to draw, jump up to VLL1
-                        \ to draw the next pixel
+ JSR PlotPixelClipped   \ Plot the left sixel of the dot at (X, Y)
 
-.RTS
+ INX                    \ Move right by one sixel
 
- RTS                    \ Return from the subroutine
+ JSR PlotPixelClipped   \ Plot the right sixel of the dot ay (X, Y)
 
-                        \ If we get here then the stick length is negative (so
-                        \ the dot is above the ellipse and the stick is below
-                        \ the dot, and we need to draw the stick downwards from
-                        \ the dot)
+ JSR MoveTo             \ Move to the right sixel, so we are ready to draw the
+                        \ stick
 
- INY                    \ We want to draw the stick downwards, so we first
-                        \ increment the row counter so that it's pointing to the
-                        \ bottom-right pixel in the dot (as opposed to the top-
-                        \ right pixel that the call to CPIX4 finished on)
+ PLA                    \ Fetch the y-coordinate of the end of the stick from
+                        \ the stack into A
 
- CPY #8                 \ If the row number in Y is less than 8, then it
- BNE P%+6               \ correctly points at the next line down, so jump to
-                        \ VLL2 to skip the following
+ CMP Y1                 \ If the y-coordinate of the dot equals the y-coordinate
+ BEQ scan1              \ of the end of the stick, then there is no stick to
+                        \ draw, so jump to scan1 to return from the subroutine
 
- LDY #0                 \ We just incremented Y down through the bottom of the
-                        \ character block, so we need to move it to the first
-                        \ row in the character below, so set Y to 0, the number
-                        \ of the first row
+ LSR A                  \ Set Y = (A / 4) + 9
+ LSR A
+ CLC
+ ADC #9
+ TAY
 
- INC SC+1               \ Increment the high byte of the screen address to move
-                        \ to the character block above
+ JSR DrawTo             \ Draw the stick
 
-.VLL2
-
- INY                    \ We want to draw the stick itself, heading downwards,
-                        \ so increment the pixel row in Y
-
- CPY #8                 \ If the row number in Y is less than 8, then it
- BNE VL2                \ correctly points at the next line down, so jump to
-                        \ VL2 to skip the following
-
- LDY #0                 \ We just incremented Y down through the bottom of the
-                        \ character block, so we need to move it to the first
-                        \ row in the character below, so set Y to 0, the number
-                        \ of the first row
-
- INC SC+1               \ Increment the high byte of the screen address to move
-                        \ to the character block above
-
-.VL2
-
- LDA X1                 \ Set A to the character row byte for the stick, which
-                        \ we stored in X1 above, and which has the same pixel
-                        \ pattern as the bottom-right pixel of the dot (so the
-                        \ stick comes out of the right side of the dot)
-
-                        \ --- Mod: Original Acornsoft code removed: ----------->
-
-\EOR (SC),Y             \ Draw the stick on row Y of the character block using
-\STA (SC),Y             \ EOR logic
-
-                        \ --- And replaced by: -------------------------------->
-
- NOP                    \ Pad the code out to the same length as in the original
- NOP
- NOP
- NOP
-
-                        \ --- End of replacement ------------------------------>
-
- INX                    \ Increment the (negative) stick height in X
-
- BNE VLL2               \ If we still have more stick to draw, jump up to VLL2
-                        \ to draw the next pixel
+.scan1
 
  RTS                    \ Return from the subroutine
+
+                        \ --- End of replacement ------------------------------>
 
 \ ******************************************************************************
 \
